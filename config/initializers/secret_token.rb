b9469d01fc512f0ef42f6565ab6cdd2ce4955c6d
Serg Sergiienko <serg.sergiienko@gmail.com>
2015-08-22 21:02:57 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clickr::Application.config.secret_key_base = 'a6c7a882f0bacfebf73479de1f6e3fd0bd9aa087c65cf7f47760ad32be99d17f6141b96c445579a08ab900d1cb62e53c05b8eae41361f418b02f502bfbab1c7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clickr::Application.config.session_store :cookie_store, key: '_clickr_session'
